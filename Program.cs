c68e4f6031b697da4b7ce483ff08b789cc185f3a
Артемида <sedikov632@gmail.com>
2026-04-05 22:31:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int result = Fibonacci(5);
+Console.WriteLine(result);
+
+/// <summary>
+/// Вычисляет N-е число последовательности Фибоначчи.
+/// </summary>
+/// <param name="n">Порядковый номер числа (начиная с 0).</param>
+/// <returns>N-е число Фибоначчи.</returns>
+static int Fibonacci(int n)
+{
+    Console.WriteLine("The output is: ");
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
+}
